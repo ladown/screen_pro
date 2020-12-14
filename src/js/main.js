0d0d494970ace3684723249edbfc846cc2a457a1
Egor <egorka33rus52@gmail.com>
2020-12-14 20:12:20 +0300

    completed header and promo

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-console.log(`Checking optimazing js`);
+window.addEventListener('DOMContentLoaded', () => {
+  // HAMBURGER MENU
+
+  const hamburgerBtn = document.querySelector(`.header__nav-hamburger`);
+  const headerMenu = document.querySelector(`.header__nav-list`);
+  const menuItem = document.querySelectorAll(`.header__nav-link`);
+
+  hamburgerBtn.addEventListener(`click`, () => {
+    headerMenu.classList.toggle(`header__nav-list-active`);
+    hamburgerBtn.classList.toggle(`header__nav-hamburger-active`);
+  });
+
+  menuItem.forEach((item) => {
+    item.addEventListener(`click`, () => {
+      headerMenu.classList.toggle(`header__nav-list-active`);
+      hamburgerBtn.classList.toggle(`header__nav-hamburger-active`);
+    });
+  });
+
+  // POPUP MENU
+
+  const arrowBtn = document.querySelector(`.header__contacts-arrowdown`);
+  const text = document.querySelector(`.header__contacts-text`);
+  const social = document.querySelector(`.header__contacts-mess`);
+
+  arrowBtn.addEventListener(`click`, () => {
+    arrowBtn.classList.toggle(`header__contacts-arrowdown-active`);
+    text.classList.toggle('header__contacts-text-active');
+    social.classList.toggle(`header__contacts-mess-active`);
+  });
+});
